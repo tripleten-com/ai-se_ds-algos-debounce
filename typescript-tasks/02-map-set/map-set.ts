295d6c2b673e9fcecc3c4a6e96a5e44bec0b2d9d
Kevin Loughead <kvnloughead@gmail.com>
2026-06-17 16:34:49 -0400

    reset tasks to starting code

--- typescript-tasks/02-map-set/map-set.ts
+++ typescript-tasks/02-map-set/map-set.ts
@@ -9,12 +9,6 @@ const countries = [
 
 // TODO: Use a Set to collect all unique log levels
 // Export it as a logLevels const
-export const logLevels = new Set(logs);
-console.log(logLevels.size);
 
 // TODO: Use a Map to build a country → capital lookup from the countries array.
 // Export it as a capitalByCountry const
-export const capitalByCountry = new Map(
-  countries.map((country) => [country.country, country.capital]),
-);
-console.log(capitalByCountry.get("Japan"));
--- typescript-tasks/03-searching/search.ts
+++ typescript-tasks/03-searching/search.ts
@@ -1,26 +1,7 @@
 export function linearSearch(items: number[], target: number): number {
-  for (let i = 0; i < items.length; i++) {
-    if (items[i] === target) {
-      return i;
-    }
-  }
   return -1;
 }
 
 export function binarySearch(sortedItems: number[], target: number): number {
-  let low = 0;
-  let high = sortedItems[sortedItems.length - 1];
-
-  while (low <= high) {
-    const mid = Math.round((high + low) / 2);
-    if (sortedItems[mid] === target) {
-      return mid;
-    } else if (sortedItems[mid] < target) {
-      low = mid + 1;
-    } else {
-      high = mid - 1;
-    }
-  }
-
   return -1;
 }
