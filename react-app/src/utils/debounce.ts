aa0c6015ed6ee40e32d9a51b6e8de902fb48a806
Kevin Loughead <kvnloughead@gmail.com>
2026-06-18 14:38:04 -0400

    add debounce starter to react app

---
+++ react-app/src/utils/debounce.ts
@@ -0,0 +1,18 @@
+export function debounceTrailing<T extends unknown[]>(
+  fn: (...args: T) => void,
+  delay: number,
+): (...args: T) => void {
+  let timerId: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: T) => {
+    if (timerId !== null) clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      fn(...args);
+      timerId = null;
+    }, delay);
+  };
+}
+
+// Copy your debounce implementation here from the previous task.
+// Rename it debounceLeading
+export function debounceLeading() {}
